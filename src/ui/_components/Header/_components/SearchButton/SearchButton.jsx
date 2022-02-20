84b5aa37413eb4578d9c8fedc1208146c885b1fc
igor-co <icojocaru-ext@tracasa.es>
2022-02-20 15:18:37 +0100

    Update search button

--- src/ui/_components/Header/_components/SearchButton/SearchButton.jsx
+++ src/ui/_components/Header/_components/SearchButton/SearchButton.jsx
@@ -1,14 +1,22 @@
 import styles from './SearchButton.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 import { config } from 'conf';
 
 import { LoupeIcon } from 'assets/icons/LoupeIcon';
 
 export const SearchButton = ({ onClick = () => {} }) => {
-  const { messages } = config;
+  const { messages, paths } = config;
+
+  const navigate = useNavigate();
+
+  const onSearch = () => {
+    onClick();
+    navigate(`/${paths.SEARCH}`);
+  };
 
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onSearch}>
       <LoupeIcon />
       <span className={styles.label}>{messages['search']}</span>
     </button>
