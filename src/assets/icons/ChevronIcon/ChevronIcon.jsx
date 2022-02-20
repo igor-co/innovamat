654a2d024830a34193d9c14795a0027e5456d0c9
igor-co <icojocaru-ext@tracasa.es>
2022-02-20 13:30:39 +0100

    Fix dev tools warnings

--- src/assets/icons/ChevronIcon/ChevronIcon.jsx
+++ src/assets/icons/ChevronIcon/ChevronIcon.jsx
@@ -1,6 +1,6 @@
 import styles from '../Icons.module.scss';
 
-export const ChevronIcon = ({ isActive }) => {
+export const ChevronIcon = ({ isActive = false }) => {
   return (
     <svg
       svg
@@ -12,8 +12,8 @@ export const ChevronIcon = ({ isActive }) => {
     >
       <path
         d='M21.3438 4.8125L11.9637 14.1925C11.8408 14.3156 11.674 14.3848 11.5 14.3848C11.326 14.3848 11.1592 14.3156 11.0362 14.1925L1.65625 4.8125'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
     </svg>
--- src/assets/icons/FiltersIcon/FiltersIcon.jsx
+++ src/assets/icons/FiltersIcon/FiltersIcon.jsx
@@ -11,36 +11,36 @@ export const FiltersIcon = ({ isActive }) => {
     >
       <path
         d='M12.735 15.75L18.735 15.75'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
       <path
         d='M5.23499 15.75L8.23499 15.75'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
       <path
         fillRule='evenodd'
         clipRule='evenodd'
         d='M10.485 18C9.24234 18 8.23499 16.9926 8.23499 15.75C8.23499 14.5074 9.24234 13.5 10.485 13.5C11.7276 13.5 12.735 14.5074 12.735 15.75C12.735 16.9926 11.7276 18 10.485 18Z'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
       <path
         d='M14.235 8.25L5.23499 8.25'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
       <path
         fillRule='evenodd'
         clipRule='evenodd'
         d='M16.485 10.5C17.7276 10.5 18.735 9.49264 18.735 8.25C18.735 7.00736 17.7276 6 16.485 6C15.2423 6 14.235 7.00736 14.235 8.25C14.235 8.84674 14.472 9.41903 14.894 9.84099C15.316 10.2629 15.8882 10.5 16.485 10.5V10.5Z'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
         strokeLinejoin='round'
       />
     </svg>
