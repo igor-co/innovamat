5bb911077031b0ebce369d6bad46347fe284ca45
igor-co <icojocaru-ext@tracasa.es>
2022-02-21 16:26:11 +0100

    Update chevron component

--- src/assets/icons/ChevronIcon/ChevronIcon.jsx
+++ src/assets/icons/ChevronIcon/ChevronIcon.jsx
@@ -1,6 +1,6 @@
 import styles from '../Icons.module.scss';
 
-export const ChevronIcon = ({ isActive = false }) => {
+export const ChevronIcon = () => {
   return (
     <svg
       svg
@@ -8,7 +8,7 @@ export const ChevronIcon = ({ isActive = false }) => {
       height='21'
       viewBox='0 0 23 21'
       fill='none'
-      className={isActive ? styles.navIconActive : styles.navIconDisabled}
+      className={styles.navIconDisabled}
     >
       <path
         d='M21.3438 4.8125L11.9637 14.1925C11.8408 14.3156 11.674 14.3848 11.5 14.3848C11.326 14.3848 11.1592 14.3156 11.0362 14.1925L1.65625 4.8125'
